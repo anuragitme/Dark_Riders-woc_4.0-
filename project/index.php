70f81026701b9ad8b9a4bf8c2eb777e8edf1ac3c
Anurag <anuragitme@gmail.com>
2021-05-18 11:32:38 +0530

    Added button for see more option

--- project/index.php
+++ project/index.php
@@ -18,79 +18,82 @@
         <?php include'partials/_navbar2.php'; ?>
 
         
-
+        <!-- Slider starts here -->
         <div class = "col-md-12" style = "padding:0;">
-            <div class = "carousel slide" data-ride = "carousel" id = "carousel1">
+                <div class = "carousel slide" data-ride = "carousel" id = "carousel1">
 
-                <!--Caraousel Indicators -->
-                <ol class = "carousel-indicators">
-                    <li data-slide-to = "0" data-target = "#carousel1" class = "active"></li>
-                    <li data-slide-to = "1" data-target = "#carousel1"></li>
-                    <li data-slide-to = "2" data-target = "#carousel1"></li>
-                    <li data-slide-to = "3" data-target = "#carousel1"></li>
-                </ol>
-                
-                <!-- Carousel Inner -->
-                <div class = "carousel-inner">
-                    <div class = "item active">
-                        <img src = "../Carousel/c1.PNG" class = "img-responsive carousel_img_0">
-                    </div>
+                    <!--Caraousel Indicators -->
+                    <ol class = "carousel-indicators">
+                        <li data-slide-to = "0" data-target = "#carousel1" class = "active"></li>
+                        <li data-slide-to = "1" data-target = "#carousel1"></li>
+                        <li data-slide-to = "2" data-target = "#carousel1"></li>
+                        <li data-slide-to = "3" data-target = "#carousel1"></li>
+                    </ol>
+                    
+                    <!-- Carousel Inner -->
+                    <div class = "carousel-inner">
+                        <div class = "item active">
+                            <img src = "../Carousel/c1.PNG" class = "img-responsive carousel_img_0">
+                        </div>
 
-                    <div class = "item ">
-                        <img src = "../Carousel/c2.PNG" class = "img-responsive carousel_img_0">
-                    </div>
+                        <div class = "item ">
+                            <img src = "../Carousel/c2.PNG" class = "img-responsive carousel_img_0">
+                        </div>
 
-                    <div class = "item ">
-                        <img src = "../Carousel/c3.PNG" class = "img-responsive carousel_img_0">
-                    </div>
+                        <div class = "item ">
+                            <img src = "../Carousel/c3.PNG" class = "img-responsive carousel_img_0">
+                        </div>
 
-                    <div class = "item ">
-                        <img src = "../Carousel/c4.PNG" class = "img-responsive carousel_img_0">
+                        <div class = "item ">
+                            <img src = "../Carousel/c4.PNG" class = "img-responsive carousel_img_0">
+                        </div>
                     </div>
+                    
+
+                    <!--Carousel Controls -->
+                    <a href = "#carousel1" class = "left carousel-control" data-slide = "prev">
+                        <span class = "glyphicon glyphicon-chevron-left"></span>
+                    </a>
+
+                    <a href = "#carousel1" class = "right carousel-control" data-slide = "next">
+                        <span class = "glyphicon glyphicon-chevron-right"></span>
+                    </a>
                 </div>
-                
+        </div>  
+ 
+         
 
-                <!--Carousel Controls -->
-                <a href = "#carousel1" class = "left carousel-control" data-slide = "prev">
-                    <span class = "glyphicon glyphicon-chevron-left"></span>
-                </a>
-
-                <a href = "#carousel1" class = "right carousel-control" data-slide = "next">
-                    <span class = "glyphicon glyphicon-chevron-right"></span>
-                </a>
-            </div>
-        </div>
         <div style = "clear:both;"></div>
         <div class="container_flex_0">
             <div class="flex_item" id="item1">
                 <h3 class = "text-center">Computer and Accessories</h3>
                 <img src = "../Images/i1.PNG" alt = "" class = "img-responsive img_flex">
-                <a href = "computer and accessories.php"><h5 class = "text-center">See More</h5></a>
+                <a href = "computer and accessories.php"><h5 class = " btn btn-info">See More</h5></a>
             </div>
             <div class="flex_item" id="item2">
                 <h3 class="text-center">Beauty Picks</h3>
                 <img src="../Images/i2.PNG" alt = "" class = "img-responsive img_flex">
-                <a href = "beauty picks.php"><h5 class = "text-center">See More</h5></a>
+                <a href = "beauty picks.php"><h5 class = "btn btn-info">See More</h5></a>
             </div>
             <div class="flex_item" id="item3">
                 <h3 class="text-center">Toy and Joy</h3>
                 <img src = "../Images/i3.PNG" alt = "" class = "img-responsive img_flex">
-                <a href = "toys.php"><h5 class = "text-center">See More</h5></a>
+                <a href = "toys.php"><h5 class = "btn btn-info">See More</h5></a>
             </div>
             <div class="flex_item" id="item4">
                 <h3 class="text-center">Latest Fashion</h3>
                 <img src = "../Images/i7.jpg" alt = "" class = "img-responsive img_flex">
-                <a href = "clothes.php"><h5 class = "text-center">See More</h5></a>
+                <a href = "clothes.php"><h5 class = "btn btn-info">See More</h5></a>
             </div>
             <div class="flex_item" id="item5">
                 <h3 class="text-center">Electronics</h3>
                 <img src = "../Images/i4.PNG" alt = "" class = "img-responsive img_flex">
-                <a href = "electronics.php"><h5 class = "text-center">See More</h5></a>
+                <a href = "electronics.php"><h5 class = "btn btn-info">See More</h5></a>
             </div>
             <div class="flex_item" id="item6">
                 <h3 class="text-center">Home Furniture</h3>
                 <img src = "../Images/i8.PNG" alt =" " class = "img-responsive img_flex">
-                <a href = "furniture.php"><h5 class = "text-center">See More</h5></a>
+                <a href = "furniture.php"><h5 class = "btn btn-info">See More</h5></a>
             </div>   
         </div>
         <div class = "footer_header_0"><h2 class="text-center">Ace Deals</h2></div>
