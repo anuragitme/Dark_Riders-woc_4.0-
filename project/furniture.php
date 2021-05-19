6fe9a36162b10df1713c9d199c3b8315a0e0db02
Anurag <anuragitme@gmail.com>
2021-05-19 15:29:00 +0530

    removed extra side pane from furniture page and added promo video to side pane

--- project/furniture.php
+++ project/furniture.php
@@ -26,23 +26,7 @@ session_start(); ?>
 
          
 
-            <ul class = "nav nav-pills nav-stacked">
-                <br>
-                <li><span class = "nav-pills-heading">&nbsp;&nbsp;Categories</span></li>
-                <li><a href = "electronics.php" class = "modal_a-grp_0">&nbsp;&nbsp;Electronics&nbsp;&nbsp;&nbsp;<span class="fa fa-microchip"></span></a></li>
-                <li><a href = "computer and accessories.php" class = "modal_a-grp_0">&nbsp;&nbsp;Computers&nbsp;&nbsp;&nbsp;<span class="fa fa-desktop"></span></a></li>
-                <li><a href = "clothes.php" class = "modal_a-grp_0">&nbsp;&nbsp;Clothing&nbsp;&nbsp;&nbsp;<span class="fa fa-female"></span></a></li>
-                <li><a href = "toys.php" class = "modal_a-grp_0">&nbsp;&nbsp;Toys&nbsp;&nbsp;&nbsp;<span class="fa fa-superpowers"></span></a></li>
-                <li><a href = "furniture.php" class = "modal_a-grp_0">&nbsp;&nbsp;Furniture&nbsp;&nbsp;&nbsp;<span class="fa fa-bed"></span></a></li>
-                <li><a href = "beauty picks.php" class = "modal_a-grp_0">&nbsp;&nbsp;Beauty Picks&nbsp;&nbsp;&nbsp;<span class="fa fa-heart"></span></a></li>
-                <li><span class = "nav-pills-heading">&nbsp;&nbsp;Digital Content and Devices</span></li>
-                <li><a href = "#" class = "modal_a-grp_0">Ace Deals Music&nbsp;&nbsp;&nbsp;<span class="fa fa-volume-up"></span></a></li>
-                <li><a href = "#" class = "modal_a-grp_0">Ace Deals Promotion Video&nbsp;&nbsp;&nbsp;<span class="fa fa-play-circle"></span></a></li>
-                <li><a href = "#" class = "modal_a-grp_0">Ace Deals Servers&nbsp;&nbsp;&nbsp;<span class="fa fa-database"></span></a></li>
-                <li><span class = "nav-pills-heading">&nbsp;&nbsp;Help and Settings</span></li>
-                <li><a href = "#" class = "modal_a-grp_0">Login Info&nbsp;&nbsp;&nbsp;<span class="fa fa-user-o"></span></a></li>
-                <li><a href = "#" class = "modal_a-grp_0">Sign Up&nbsp;&nbsp;&nbsp;<span class="glyphicon glyphicon-log-in"></span></a></li>                                                          
-            </ul>
+            
         </div>
         <div class = "vl"></div>
         <div class = "main_body">
--- project/partials/_side_pane.php
+++ project/partials/_side_pane.php
@@ -12,7 +12,7 @@ echo' <div class = "side_pane">
                 <li><a href = "beauty picks.php" class = "modal_a-grp_0">&nbsp;&nbsp;Beauty Picks&nbsp;&nbsp;&nbsp;<span class="fa fa-heart"></span></a></li>
                 <li><span class = "nav-pills-heading">&nbsp;&nbsp;Digital Content and Devices</span></li>
                 <li><a href = "#" class = "modal_a-grp_0">Ace Deals Music&nbsp;&nbsp;&nbsp;<span class="fa fa-volume-up"></span></a></li>
-                <li><a href = "#" class = "modal_a-grp_0">Ace Deals Promotion Video&nbsp;&nbsp;&nbsp;<span class="fa fa-play-circle"></span></a></li>
+                <li><a href = "promo_video.php" class = "modal_a-grp_0">Ace Deals Promotion Video&nbsp;&nbsp;&nbsp;<span class="fa fa-play-circle"></span></a></li>
                 <li><a href = "#" class = "modal_a-grp_0">Ace Deals Servers&nbsp;&nbsp;&nbsp;<span class="fa fa-database"></span></a></li>
                 <li><span class = "nav-pills-heading">&nbsp;&nbsp;Help and Settings</span></li>
                 <li><a href = "#" class = "modal_a-grp_0">Login Info&nbsp;&nbsp;&nbsp;<span class="fa fa-user-o"></span></a></li>
